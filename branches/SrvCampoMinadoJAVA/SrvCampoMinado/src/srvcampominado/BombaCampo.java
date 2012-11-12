b32d68d5a4c189ab07210c1800b64ee93486f02a
antonionyceles@gmail.com <antonionyceles@gmail.com@45435d69-fb89-9cdd-ea94-e06151e6b6b4>
2012-11-12 12:03:20 +0000

    Alterando Pasta do repositório

---
+++ branches/SrvCampoMinadoJAVA/SrvCampoMinado/src/srvcampominado/BombaCampo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package srvcampominado;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author antoniony.celes
+ */
+public interface BombaCampo extends Remote{
+    
+    public void conteudoCampo() throws RemoteException;
+}
