ec3f90658055690476c6bcde6679aef61ddbfdf0
antonionyceles@gmail.com <antonionyceles@gmail.com@45435d69-fb89-9cdd-ea94-e06151e6b6b4>
2012-11-12 12:01:04 +0000

    First import

---
+++ trunk/src/srvcampominado/Bomba.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package srvcampominado;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author antoniony.celes
+ */
+public class Bomba extends UnicastRemoteObject implements BombaCampo{
+
+    private int[][] bomba;
+    
+    public Bomba() throws RemoteException{
+        
+    }
+    @Override
+    public void conteudoCampo() throws RemoteException {
+        this.bomba = new int[5][5];
+    }
+    
+}
---
+++ trunk/src/srvcampominado/SrvCampoMinado.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package srvcampominado;
+
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+
+  
+  
+public class SrvCampoMinado 
+{  
+  public static void main(String args[])  
+    throws RemoteException,MalformedURLException{  
+       LocateRegistry.createRegistry(1099);
+        Campo campo = new Campo();
+        Naming.rebind("Jogo", campo);
+        System.out.println("Service ONLINE");
+    }  
+}   
+/**
+ *
+ * @author Administrator
+ */
+
